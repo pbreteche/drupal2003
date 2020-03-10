b48028dba4d84996a9915805e2d850e5be2c17f9
Pierre Bretéché <pbreteche@dawan.fr>
2020-03-10 15:14:42 +0100

    paramétrage du block

--- web/modules/forecast/src/Plugin/Block/ExampleBlock.php
+++ web/modules/forecast/src/Plugin/Block/ExampleBlock.php
@@ -3,6 +3,7 @@
 namespace Drupal\forecast\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * @Block(
@@ -13,14 +14,37 @@ use Drupal\Core\Block\BlockBase;
  */
 class ExampleBlock extends BlockBase {
 
+  const CITY_DEFAULT_VALUE = 'France';
+
   /**
    * {@inheritdoc}
    */
   public function build() {
+    $city = $this->getConfiguration()['city'];
+    if (empty($city)) {
+      $city = self::CITY_DEFAULT_VALUE;
+    }
+
     $build['content'] = [
-      '#markup' => $this->t('It works!'),
+      '#markup' => $this->t('La météo de '.$city),
     ];
     return $build;
   }
 
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form['city'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('city'),
+      '#default_value' => $this->getConfiguration()['city'],
+    ];
+
+    return $form;
+  }
+
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->setConfigurationValue(
+      'city',
+      $form_state->getValue('city')
+    );
+  }
 }
